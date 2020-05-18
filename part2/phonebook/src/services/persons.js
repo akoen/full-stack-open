63c5533db8a126433bf269f05f163363c7903431
akoen <arw.koen@gmail.com>
2020-05-18 12:53:08 -0700

    Phonebook step10 - services

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/persons';
+
+const getPersons = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const addPerson = (personObject) => {
+  return axios.post(baseUrl, personObject).then((response) => response.data);
+};
+
+const deletePerson = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+const replaceNumber = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then((response) => response.data);
+};
+
+export default { getPersons, addPerson, deletePerson, replaceNumber };
